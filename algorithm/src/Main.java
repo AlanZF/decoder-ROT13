067f029e4c88a561668bc32d0ca0949ec97e510a
Alan <alan.stfreitas@gmail.com>
2023-07-07 11:59:46 -0300

    Separating findIndex/convLetter methods and creating diagram

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     final static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
@@ -8,22 +11,30 @@ public class Main {
 
     // Separar em 2 métodos: findIndex, que retorna só o index
     // e convLetter, que converte os indexes nas letras correspondentes
-    public static void findIndex(String message) {
+    List<String> indexes = new ArrayList<>();
+    int idx = 0;
+    public List<String> findIndex(String message) {
         String[] a = convText(alphabet);
         String[] m = convText(message);
-        String convLetter;
+        //String convLetter;
         for(int i = 0; i < m.length; i++) {
-            int idx = alphabet.indexOf(m[i]);
-            if(idx == -1) {
-                convLetter = "";
-            } else {
-                convLetter = a[idx+13];
-            }
-            System.out.println(convLetter);
+            idx = alphabet.indexOf(m[i]);
+            indexes.add(Integer.toString(idx));
         }
+        return indexes;
     }
 
+    /*public void convLetter() {
+        if(idx == -1) {
+            convLetter = "";
+        } else {
+            convLetter = a[idx+13];
+        }
+        System.out.println(convLetter);
+    }*/
+
     public static void main(String[] args) {
-        findIndex("URYYB JBEYQ");
+        Main m = new Main();
+        System.out.println(m.findIndex("URYYB JBEYQ"));
     }
 }
