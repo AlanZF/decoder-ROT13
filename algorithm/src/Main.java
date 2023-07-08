a94003febf7f84c0af799795321a598960fff621
Alan <alan.stfreitas@gmail.com>
2023-07-07 23:56:21 -0300

    Creating encodeDecode method

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,40 +1,43 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     final static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    public static String[] convText(String text) {
+    public static String[] convertStringToArray(String text) {
         String[] convText = text.split("");
         return convText;
     }
 
-    // Separar em 2 métodos: findIndex, que retorna só o index
-    // e convLetter, que converte os indexes nas letras correspondentes
-    List<String> indexes = new ArrayList<>();
-    int idx = 0;
-    public List<String> findIndex(String message) {
-        String[] a = convText(alphabet);
-        String[] m = convText(message);
-        //String convLetter;
-        for(int i = 0; i < m.length; i++) {
-            idx = alphabet.indexOf(m[i]);
-            indexes.add(Integer.toString(idx));
+    public List<Integer> findIndex(String message) {
+        String[] msg = convertStringToArray(message);
+        List<Integer> indexes = new ArrayList<>();
+
+        for(int i = 0; i < msg.length; i++) {
+            int index = alphabet.indexOf(msg[i]);
+            Integer idx = index;
+            indexes.add(idx);
         }
         return indexes;
     }
 
-    /*public void convLetter() {
-        if(idx == -1) {
-            convLetter = "";
-        } else {
-            convLetter = a[idx+13];
-        }
-        System.out.println(convLetter);
-    }*/
+    public String decodeEncode(String message) {
+        List<Integer> indexes = findIndex(message);
+        String[] a = convertStringToArray(alphabet);
+        String[] letter = new String[indexes.size()];
 
+        for(int i = 0; i < indexes.size(); i++) {
+            if(indexes.get(i) == -1) {
+                letter[i] = "";
+            } else {
+                letter[i] = a[indexes.get(i)+13];
+            }
+        }
+        return Arrays.toString(letter);
+    }
     public static void main(String[] args) {
-        Main m = new Main();
-        System.out.println(m.findIndex("URYYB JBEYQ"));
+        Main main = new Main();
+        System.out.println(main.decodeEncode("URYYB JBEYQ"));
     }
 }
