18208de4551a8d25e796059ce6c5f192c6292be2
Alan <alan.stfreitas@gmail.com>
2023-07-09 21:52:29 -0300

    Initializing the Spring Boot project

---
+++ api/decoder-api/src/test/java/com/alanzf/decoderapi/DecoderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanzf.decoderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecoderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
